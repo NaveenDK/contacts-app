444c40c48eb7b2e4a0f9094b57872854e7857a4c
naveendk <naveenkolambage@gmail.com>
2020-06-21 17:33:27 +1200

    added navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,8 +3,10 @@ import PropTypes from 'prop-types';
 
 const Navbar = ({title,icon})=>{
     return(
-        <div>
-
+        <div className="navbar bg-primary">
+           <h1>
+                <i className={icon}/> {title} 
+           </h1>
         </div>
     )
 }
@@ -13,5 +15,8 @@ Navbar.propTypes = {
     title: PropTypes.string.isRequired,
     icon:PropTypes.string,
 }
-
+Navbar.defaultProps = {
+    title: 'Contact Keeper',
+    icon:'fas fa-id-card-alt'
+}
 export default Navbar
